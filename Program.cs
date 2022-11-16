1f1cc11fc57af1a94b424c8d3c197465220853e5
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-11-16 18:14:40 -0300

    ganhei 50 reais

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Vamos lá... cite os números inteiros entre 1 e 40 inclusive, substituindo os múltiplos de 4 pela palavra pim. ");
+Console.ReadKey();
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Green;
+for (int numero = 1; numero <=40; numero++)
+Console.Write(numero % 4 == 0 ? "pim\n" : $"{numero,2} ");
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("você ganhou... 50 reais!!! hihiiii");
+Console.ResetColor();
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # UmDoisTresPim
 quem quer dinheiro? hihiiii.
+
+O apresentador de auditório Silvio Santos em seus programas de domingo à tarde oferecia dinheiro para quem conseguisse citar os números inteiros entre 1 e 40 (inclusive) substituindo os múltiplos de 4 pela palavra pim.
+
+Faça um programa que exiba a sequência correta.
+
+Exemplo:
+
+ 1  2  3 pim
+ 5  6  7 pim
+ 9 10 11 pim
+...
+33 34 35 pim
+37 38 39 pim
